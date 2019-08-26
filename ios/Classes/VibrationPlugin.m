d5f2ac3ce8e145a226898b7f97c01cecaf94413b
Hugo Guo <fir@HugodeMBP-2.lan>
2019-08-26 18:02:23 +0800

    delete ios

--- ios/Classes/VibrationPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "VibrationPlugin.h"
-#import <vibration/vibration-Swift.h>
-
-@implementation VibrationPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftVibrationPlugin registerWithRegistrar:registrar];
-}
-@end
